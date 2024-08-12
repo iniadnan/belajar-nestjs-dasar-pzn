6addcedc740cca3cbe5a6e19999344e31a5d0328
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2024-08-12 21:50:26 +0700

    Checkpoint: View

---
+++ src/user/user/user.controller.ts
@@ -0,0 +1,69 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Header,
+  HttpCode,
+  Req,
+  Res,
+  HttpRedirectResponse,
+  Redirect,
+} from '@nestjs/common';
+import { Response, Request } from 'express';
+
+@Controller('/api/users')
+export class UserController {
+  @Get('/set-cookie')
+  setCookie(@Query('name') name: string, @Res() response: Response) {
+    response.cookie('name', name);
+    response.status(200).send('Success Set Cookie');
+  }
+
+  @Get('/get-cookie')
+  getCookie(@Req() request: Request): string {
+    return request.cookies['name'];
+  }
+
+  @Get('/sample-response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    return {
+      data: 'Hello Json',
+    };
+  }
+
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return {
+      url: '/api/users/sample-response',
+      statusCode: 301,
+    };
+  }
+
+  @Get('/hello')
+  async sayHello(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName: string,
+  ): Promise<string> {
+    return `GET: Hello ${firstName} - ${lastName}`;
+  }
+
+  @Get('/:id')
+  getById(@Param('id') id: string): string {
+    return `GET: ${id}`;
+  }
+
+  @Post()
+  post(): string {
+    return 'POST';
+  }
+
+  @Get('/sample')
+  get(): string {
+    return 'GET';
+  }
+}
