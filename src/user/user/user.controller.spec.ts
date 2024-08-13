9326bf88b1cd957b621503d11bb2d14054b02ea7
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2024-08-13 22:50:17 +0700

    Checkpoint: Module Reference

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import * as httpMock from 'node-mocks-http';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -8,14 +9,15 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [UserService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
   });
 
   it('should can be say hello', async () => {
-    const response = await controller.sayHello('Muhamad', 'Adnan');
-    expect(response).toBe('Hello Muhamad - Adnan');
+    const response = await controller.sayHello('Muhamad');
+    expect(response).toBe('Hello Muhamad');
   });
 
   it('should can view template', async () => {
